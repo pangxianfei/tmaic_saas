5616625ce05bc2a169ff0073116a7888a1b67a51
pangxianfei <421339244@qq.com>
2022-03-03 17:28:30 +0800

    saas性能优化,使用map方式

---
+++ app/sassmodels/user.go
@@ -0,0 +1,16 @@
+package sassmodels
+
+import (
+	"gitee.com/pangxianfei/frame/helpers/zone"
+)
+
+type SaasUser struct {
+	ID        int64     `gorm:"column:user_id;primary_key;auto_increment"`
+	Name      string    `gorm:"column:user_name;type:varchar(100)"`
+	TenantsId int64     `gorm:"column:tenants_id;type:int unsigned"`
+	Email     string    `gorm:"column:user_email;type:varchar(100);unique_index;not null"`
+	Password  string    `gorm:"column:user_password;type:varchar(100);not null;" json:"-"`
+	CreatedAt zone.Time `gorm:"column:user_created_at"`
+	UpdatedAt zone.Time `gorm:"column:user_updated_at"`
+	DeletedAt zone.Time `gorm:"column:user_deleted_at"`
+}
